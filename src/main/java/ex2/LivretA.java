f8e93a43ab6971b770235716d1d3db0ddb60fce3
Quentin Fitamant <quentinfitamantp@gmail.com>
2024-11-14 16:13:09 +0100

    Ex2

--- src/main/java/ex2/LivretA.java
+++ src/main/java/ex2/LivretA.java
@@ -2,8 +2,48 @@ package ex2;
 
 public class LivretA extends CompteBancaire {
 
+	/** tauxRemuneration : taux de rémunération*/
+	private double tauxRemuneration;
+
+	/** Constructeur
+	 *
+	 * @param type
+	 * @param solde
+	 * @param tauxRemuneration
+	 */
 	public LivretA(String type, double solde, double tauxRemuneration) {
-		super(type, solde, 0, tauxRemuneration);
+		super(type, solde, 0);
+		this.tauxRemuneration = tauxRemuneration;
 	}
 
+	/** Méthode qui override celle de la classe mère car un livretA ne peut avoir de découvert
+	 *
+	 * @param montant
+	 */
+	@Override
+	public void debiterMontant(double montant) {
+		if (this.getSolde() - montant > 0){
+			this.setSolde(this.getSolde() - montant);
+		}
+	}
+
+	/** Méthode qui applique la rémuneration annuelle du livret A
+	 *
+	 */
+	public void appliquerRemuAnnuelle(){
+		this.setSolde(this.getSolde() + this.getSolde()*tauxRemuneration/100);
+	}
+
+	/** Getter for tauxRemuneration
+	 * @return the tauxRemuneration
+	 */
+	public double getTauxRemuneration() {
+		return tauxRemuneration;
+	}
+	/** Setter
+	 * @param tauxRemuneration the tauxRemuneration to set
+	 */
+	public void setTauxRemuneration(double tauxRemuneration) {
+		this.tauxRemuneration = tauxRemuneration;
+	}
 }
