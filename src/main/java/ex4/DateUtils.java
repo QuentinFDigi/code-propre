5776a5e0a5c7e9f8022f92a7d30815c41520ac50
Quentin Fitamant <quentinfitamantp@gmail.com>
2024-11-14 16:36:14 +0100

    Ex4

--- src/main/java/ex4/DateUtils.java
+++ src/main/java/ex4/DateUtils.java
@@ -2,18 +2,22 @@ package ex4;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Objects;
 
+/** CLasse permettant de formatter une date
+ *
+ */
 public class DateUtils {
 
+	/** Méthode permattant d'appliquer une format de date personnalisé ou un format part défaut si aucun patterne n'a été envoyer.
+	 *
+	 * @param pattern
+	 * @param date
+	 * @return
+	 */
 	public static String format(String pattern, Date date) {
 		
-		SimpleDateFormat formateur = new SimpleDateFormat(pattern);
-		return formateur.format(date);
-	}
-	
-	public static String formatDefaut(Date date) {
-		
-		SimpleDateFormat formateur = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		SimpleDateFormat formateur = new SimpleDateFormat((Objects.equals(pattern, "")) ? "dd/MM/yyyy HH:mm:ss" : pattern);
 		return formateur.format(date);
 	}
 }
